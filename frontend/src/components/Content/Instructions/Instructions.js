0a096b14272b9f65c654853df3c18a07803a7e9a
huniversal <hunivers0523@naver.com>
2024-11-09 17:07:32 +0900

    부분 수정

--- frontend/src/components/Content/Instructions/Instructions.js
+++ frontend/src/components/Content/Instructions/Instructions.js
@@ -1,30 +1,3 @@
-// import React, { useState } from "react";
-// import InputForm from "./InputForm";
-// import OutputDisplay from "./OutputDisplay";
-// // import "./style/Instructions.css";
-
-// const Instructions = () => {
-//   const [outputs, setOutputs] = useState([]);
-
-//   const handleCreate = (newOutput) => {
-//     setOutputs([...outputs, newOutput]);
-//   };
-
-//   const handleDelete = (index) => {
-//     const updatedOutputs = outputs.filter((_, i) => i !== index);
-//     setOutputs(updatedOutputs);
-//   };
-
-//   return (
-//     <div className="content">
-//       <InputForm onCreate={handleCreate} />
-//       <OutputDisplay outputs={outputs} onDelete={handleDelete} />
-//     </div>
-//   );
-// };
-
-// export default Instructions
-
 import React, { useState } from "react";
 import InputForm from "./InputForm";
 import OutputDisplay from "./OutputDisplay";
