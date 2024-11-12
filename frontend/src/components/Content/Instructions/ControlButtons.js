e7219d3fc408b34276541af4fe0a79fc28bc235a
huniversal <hunivers0523@naver.com>
2024-11-12 14:10:33 +0900

    사운드 에러 해결

--- frontend/src/components/Content/Instructions/ControlButtons.js
+++ frontend/src/components/Content/Instructions/ControlButtons.js
@@ -1,19 +1,54 @@
+// import React from 'react';
+// import {Howl, Howler} from 'howler';
+// import '../Instructions/style/ControlButtons.css';
+// // import './Instructions/ControlButtons.mp3';
+
+// const ControlButtons = () => {
+//   const handlePlaySound = () => {
+//     const sound = new Howl({
+//       src: [  
+//         './ControlButtons.mp3', 
+//         // './ControlButtons.webm',
+//     ], 
+//       format: [
+//         'webm',
+//         'mp3'
+//       ],
+//       volume : 1.0, // 볼륨 크기 설정 (0.0 ~ 1.0)
+//       html5  : true,
+//       onend  : () => {
+//         console.log("start!")
+//       },
+//       onloaderror: (id, error) => {
+//         console.error('오디오 로드 오류:', error);
+//       },
+//       onplayerror: (id, error) => {
+//         console.error('오디오 재생 오류:', error);
+//       }
+//     });
+//     sound.play();
+//   }
+
+//   return (
+//     <div className="Button">
+//       <button onClick={handlePlaySound} className="sound-button">
+//         호출하기
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default ControlButtons;
 import React from 'react';
-import {Howl, Howler} from 'howler';
+import {Howl} from 'howler';
 import '../Instructions/style/ControlButtons.css';
-// import './Instructions/ControlButtons.mp3';
 
 const ControlButtons = () => {
-  const handlePlaySound = () => {
     const sound = new Howl({
-      src: [
-        '../../Content/Instructions/ControlButtons.webm',        
-        '../../Content/Instructions/ControlButtons.mp3',
-        './ControlButtons.mp3', 
-        './ControlButtons.webm'
-    ], 
+      src: [  
+        '/ControlButtons.mp3', 
+      ], 
       format: [
-        'webm',
         'mp3'
       ],
       volume : 1.0, // 볼륨 크기 설정 (0.0 ~ 1.0)
@@ -28,8 +63,10 @@ const ControlButtons = () => {
         console.error('오디오 재생 오류:', error);
       }
     });
-    sound.play();
-  }
+
+    const handlePlaySound = () => {
+      sound.play();
+    }
 
   return (
     <div className="Button">
